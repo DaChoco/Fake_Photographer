6e54a8baa1eb951d5f3e54c9860f5fc0aa39189f
DaChoko <Mailechokoe2@gmail.com>
2024-12-09 19:59:17 +0200

    added slide in animations, will improve them later

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 const home = document.getElementById('home-page');
 const about = document.getElementById('about-page');
+const portfolio = document.getElementById('portfolio-page');
 
-const navlinks = document.querySelectorAll('navbar-links')
 
 const homeSect = document.getElementById('hero');
-const aboutSect = document.getElementById('About me')
+const aboutSect = document.getElementById('About-me')
+const portSect = document.getElementById('Portfolio')
 
 
 document.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('scroll', ()=>{
         let viewingHome = homeSect.getBoundingClientRect();
         let viewingAbout = aboutSect.getBoundingClientRect();
+        let viewingPort = portSect.getBoundingClientRect();
 
-        if (viewingHome.bottom >= 0 && viewingHome.top <= window.innerHeight){
+        if (viewingHome.bottom > 0 && viewingHome.top < window.innerHeight){ //hero section 
             console.log('This element is visible');
-
-            
+            home.style.setProperty('--after-width', '100%');
         }
         else{
             console.log('This element is not visible');
+            home.style.setProperty('--after-width', '0%');
+            
         }
 
-        if (viewingAbout.bottom>=0 && viewingHome.top <= window.innerHeight){
-            console.log('You can see about');
+        if (viewingAbout.bottom>0 && viewingAbout.top < (window.innerHeight)){ //about section
+            about.style.setProperty('--after-width', '100%');
         }
         else{
-            console.log('You cannot');
+            about.style.setProperty('--after-width', '0%');
         }
+
+        if (viewingPort.bottom>0 && viewingPort.top < (window.innerHeight)){ //portfolio photos section
+            portfolio.style.setProperty('--after-width', '100%');
+        }
+        else{
+            portfolio.style.setProperty('--after-width', '0%');
+        }
+
+
     })
+
+    const containers = document.querySelectorAll('.contact-container');
+    const textContainer = document.querySelector('.text-container');
+
+    textContainer.classList.add('activated');
+
+    containers.forEach((division) =>{
+       
+        division.classList.add('activated');
+
+    });
 })
